60dce65b28cfcfad5679402da84e5dd28e396730
IainYa <i.yarnall@gmail.com>
2025-01-25 13:51:00 +0000

    Unnecessary includes removed.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -16,10 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include <Arduino.h>
-#include <Hash.h>
 #include "Clock.h"
-
 #include <coredecls.h> 
 
 Clock c;
